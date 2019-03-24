30e3a70e02e6403a3c2cbe1e6f7db4e6f616b760
heartangle <1623520966@qq.com>
2019-03-24 22:18:04 +0800

    第二次提交，完成central cache层的代码，并完成测试，通过
    完成central cache层的代码，通过伪造page cache层的假数据，联通测试thread cache和central cache，功能正常

--- ConcurrentMemoryPool1.0/CentralCache.h
+++ ConcurrentMemoryPool1.0/CentralCache.h
@@ -1,7 +1,24 @@
 #pragma once
-
+#include"Common.h"
+//设计为单例模式
 class CentralCache
 {
+public:
+	static CentralCache* GetInstance()
+	{
+		return &_inst;
+	}
+	//从page cache获取一个span
+	Span* GetOneSpan(SpanList& spanlist, size_t byte_size);
+	//从中心缓存获取一定数量的对象给thread cache
+	size_t FetchRangeObj(void*& start, void*& end, size_t n, size_t byte_size);
+	//将一定数量的对象释放到span跨度
+	void ReleaseListToSpans(void* statr, size_t size);
 private:
-
+	SpanList _spanlist[NLISTS];
+private:
+	CentralCache()
+	{}
+	CentralCache(CentralCache&) = delete;
+	static CentralCache _inst;
 };
--- ConcurrentMemoryPool1.0/UnitTest.cpp
+++ ConcurrentMemoryPool1.0/UnitTest.cpp
@@ -1,63 +1,116 @@
-#include "Common.h"
-#include "ConcurrentAlloc.h"
+#pragma once
+#include"Common.h"
+#include"ConcurrentAlloc.h"
+#include"PageCache.h"
 
 void TestSize()
 {
-	cout << SizeClass::Index(10) << endl;
-	cout << SizeClass::Index(16) << endl;
+	cout << SizeClass::Index(5) << endl;
 
 	cout << SizeClass::Roundup(10) << endl;
-	cout << SizeClass::Roundup(12) << endl;
-	cout << SizeClass::Roundup(16) << endl;
-	cout << SizeClass::Roundup(17) << endl;
 }
 
 void Alloc(size_t n)
 {
 	std::vector<void*> v;
-	for (size_t i = 0; i < n; ++i)
+	for (size_t i = 0; i < n; i++)
 	{
 		v.push_back(ConcurrentAlloc(10));
 	}
-
-	for (size_t i = 0; i < n; ++i)
+	for (size_t i = 0; i < n; i++)
 	{
 		ConcurrentFree(v[i], 10);
 		cout << v[i] << endl;
 	}
 	v.clear();
-
 	cout << endl << endl;
-	for (size_t i = 0; i < n; ++i)
+	for (size_t i = 0; i < n; i++)
 	{
 		v.push_back(ConcurrentAlloc(10));
 	}
-
-	for (size_t i = 0; i < n; ++i)
+	for (size_t i = 0; i < n; i++)
 	{
 		ConcurrentFree(v[i], 10);
 		cout << v[i] << endl;
 	}
 	v.clear();
 }
+
 
 void TestThreadCache()
 {
-	std::thread t1(Alloc, 5);
-	//std::thread t2(Alloc, 5);
-	//std::thread t3(Alloc, 5);
-	//std::thread t4(Alloc, 5);
+	std::thread t1(Alloc, 1000);
+	std::thread t2(Alloc, 1000);
+	std::thread t3(Alloc, 1000);
+	std::thread t4(Alloc, 1000);
 
 	t1.join();
-	//t2.join();
-	//t3.join();
-	//t4.join();
+	t2.join();
+	t3.join();
+	t4.join();
 
 }
 
-int main()
+void TestCentralCache()
 {
-	TestThreadCache();
+	std::vector<void*> v;
+	for (size_t i = 0; i < 8; ++i)
+	{
+		v.push_back(ConcurrentAlloc(10));
+	}
 
-	return 0;
+	for (size_t i = 0; i < 8; ++i)
+	{
+		//ConcurrentFree(v[i], 10);
+		cout << v[i] << endl;
+	}
 }
+
+void TestPageCache()
+{
+	PageCache::GetInstance()->NewSpan(2);
+}
+
+void TestConcurrentAllocFree()
+{
+	size_t n = 10000;
+	std::vector<void*> v;
+	for (size_t i = 0; i < n; i++)
+	{
+		void* ptr = ConcurrentAlloc(12);
+		v.push_back(ptr);
+		printf("alloc obj:%d->%p\n", i, ptr);
+	}
+	for (size_t i = 0; i < n; i++)
+	{
+		ConcurrentFree(v[i], 12);
+		printf("free obj:%d->%p\n", i, &v[i]);
+	}
+	v.clear();
+	printf("*************************\n");
+	for (size_t i = 0; i < n; i++)
+	{
+		void* ptr = ConcurrentAlloc(12);
+		v.push_back(ptr);
+		printf("alloc obj:%d->%p\n", i, ptr);
+	}
+	for (size_t i = 0; i < n; i++)
+	{
+		ConcurrentFree(v[i], 12);
+		printf("free obj:%d->%p\n", i, v[i]);
+	}
+	v.clear();
+	printf("*************************\n");
+}
+
+//int main()
+//{
+//	//TestSize();
+//	//Alloc(10);
+//	//TestThreadCache();
+//	//TestCentralCache();
+//	//TestPageCache();
+//	//TestConcurrentAllocFree();
+//	system("pause");
+//	return 0;
+//}
